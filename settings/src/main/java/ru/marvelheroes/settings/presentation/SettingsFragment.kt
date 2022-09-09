27ca034dc9e972f2c05f3cfb4e9355b42cb92c09
Aleksandr Nikonorov <coolhabit92@gmail.com>
2022-09-09 10:25:35 +0300

    MHC-7,8: settings - switch day night added, datastore - to be

--- settings/src/main/java/ru/marvelheroes/settings/presentation/SettingsFragment.kt
+++ settings/src/main/java/ru/marvelheroes/settings/presentation/SettingsFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
 import ru.marvelheroes.presentation.base.BaseFragment
 import ru.marvelheroes.settings.R
 import ru.marvelheroes.settings.databinding.FragmentSettingsBinding
@@ -19,5 +21,17 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
     ): View? {
         binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.themeSwitchBtn.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 }
