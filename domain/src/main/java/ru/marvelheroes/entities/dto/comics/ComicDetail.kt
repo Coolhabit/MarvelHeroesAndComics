baa81a45aab4c43bee0063d234c977ca09fbdb84
Aleksandr Nikonorov <coolhabit92@gmail.com>
2022-09-16 00:00:08 +0300

    MHC-9,10: comic detail crash fixed

--- domain/src/main/java/ru/marvelheroes/entities/dto/comics/ComicDetail.kt
+++ domain/src/main/java/ru/marvelheroes/entities/dto/comics/ComicDetail.kt
@@ -3,7 +3,7 @@ package ru.marvelheroes.entities.dto.comics
 data class ComicDetail(
     val comicId: String,
     val comicName: String,
-    val comicDescription: String,
+    val comicDescription: String?,
     val comicPoster: String,
     val comicImages: List<ComicImage>
 )
